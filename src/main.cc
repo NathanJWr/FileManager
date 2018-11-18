4b7262b06d546ea132930e1dc6c44a6882f1db4f
NathanJWr <nathanielwright@protonmail.com>
2018-11-18 10:42:42 -0800

    trying to not call Texture classes destructor when pusing or emplacing back into a vector. It's not compleatly working so I'll look into it later

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include "dirobject.h"
 #include "filemanager.h"
 #include "display.h"
+#include "texture.h"
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <memory>
@@ -10,21 +11,18 @@ int main() {
   auto list = fmanager.listCurrentDirectory();
 	Display display(1024, 768);
 	display.init();
-	std::vector<SDL_Texture*> tex_list;
+	std::vector<Texture> tex_list;
 	SDL_Delay(5000);
 	for(int i = 0; i < 900; i++) {
   	for (auto n : list) {
     	std::cout << n << std::endl;
-			tex_list.push_back(display.createTextTexture(n.name, w));
+			tex_list.emplace_back(std::move(display.createTextTexture(n.name, w)));
   	}
 	}
 	SDL_Rect dest = {0, 0, 70, 20};
 	for (unsigned int i = 0; i < tex_list.size(); i++) {
-		display.renderTexture(tex_list[i], nullptr, &dest);
+		display.renderTexture(tex_list[i].get(), nullptr, &dest);
 		dest.y += 20;
-	}
-	for (unsigned int i = 0; i < tex_list.size(); i++) {
-		SDL_DestroyTexture(tex_list[i]);
 	}
 	tex_list.clear();
 	display.update();
