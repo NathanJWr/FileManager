2b2ff497cee1a2aa5a0f2eb8c1ac091a9056ec32
NathanJWr <NathanielWright@protonmail.com>
2018-12-26 12:17:00 -0800

    Refined the event handling process
    The main method now waits for events to occur and then processes them. This eliminates the need to call SDL_Delay at the end of the main loop which made navigating Folders feel too slow.

--- Windows/FileManager/main.cpp
+++ Windows/FileManager/main.cpp
@@ -12,50 +12,56 @@ public:
 	bool exit;
 	Context() : redraw(0), exit(0) {}
 };
-Context handleInput(Filesystem &dirs, ShortcutBar &bar) {
-	SDL_Event e;
+
+/* returns 1 on exit */
+int handleKeys(SDL_KeyboardEvent &e, Filesystem &dirs) {
+	switch (e.keysym.sym) {
+		case SDLK_j:
+			dirs.currentDir().moveSelectedDown();
+			break;
+		case SDLK_k:
+			dirs.currentDir().moveSelectedUp();
+			break;
+		case SDLK_l:
+			dirs.forward();
+			break;
+		case SDLK_h:
+			dirs.back();
+			break;
+		case SDLK_a:
+			dirs.toggleSortAlphabetically();
+			break;
+		case SDLK_q:
+			return 1;
+	}
+	return 0;
+}
+
+void handleMouse(Filesystem &dirs, ShortcutBar &bar) {
+	std::string path;
+	int mouse_x, mouse_y;
+	SDL_GetMouseState(&mouse_x, &mouse_y);
+	if (bar.checkClicks(mouse_x, mouse_y, path)) {
+		dirs.clean();
+		dirs = Filesystem(path);
+	}
+}
+
+Context handleInput(SDL_Event &e, Filesystem &dirs, ShortcutBar &bar) {
 	Context ctx;
-	while (SDL_PollEvent(&e)) {
-		if (e.type == SDL_QUIT) {
+	switch (e.type) {
+		case SDL_QUIT:
 			ctx.exit = true;
-		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
-			int mouse_x, mouse_y;
-			SDL_GetMouseState(&mouse_x, &mouse_y);
-			std::string path;
-			if (bar.checkClicks(mouse_x, mouse_y, path)) {
-				dirs = Filesystem(path);
-			}
+			break;
+		case SDL_MOUSEBUTTONDOWN:
 			ctx.redraw = true;
-		}
-		if (e.type == SDL_KEYDOWN) {
+			handleMouse(dirs, bar);
+			break;
+		case SDL_KEYDOWN:
 			ctx.redraw = true;
-			switch (e.key.keysym.sym) {
-			case SDLK_j:
-				//move down in same directory
-				dirs.currentDir().moveSelectedDown();
-				break;
-			case SDLK_k:
-				//move up in same directory
-				dirs.currentDir().moveSelectedUp();
-				break;
-			case SDLK_l:
-				//move into folder or open file
-				dirs.forward();
-				break;
-			case SDLK_h:
-				//move back a directory
-				dirs.back();
-				break;
-			case SDLK_a:
-				dirs.toggleSortAlphabetically();
-				break;
-			case SDLK_q:
-				//quit
+			if (handleKeys(e.key, dirs) == 1) {
 				ctx.exit = true;
-				break;
 			}
-		}
 	}
 	return ctx;
 }
@@ -70,8 +76,12 @@ int wmain(){
 	display.renderDirectory(dirs.currentDir());
 	display.renderUI(shortcut_bar);
 	display.update();
+
+	SDL_Event e;
 	while (1) {
-		Context ctx = handleInput(dirs, shortcut_bar);
+		SDL_WaitEvent(&e);
+		Context ctx = handleInput(e, dirs, shortcut_bar);
+		SDL_PollEvent(&e);
 		if (ctx.redraw) {
 			display.buildDirectory(dirs.currentDir());
 			display.renderDirectory(dirs.currentDir());
@@ -81,7 +91,6 @@ int wmain(){
 		if (ctx.exit) {
 			break;
 		}
-		SDL_Delay(100);
 	}
 	shortcut_bar.clean();
 	return 0;
