8303bfbee3a047a86f38df731b52e0d6bf3164ad
NathanJWr <NathanielWright@protonmail.com>
2018-12-26 08:12:26 -0800

    Cleaned up the display header file

--- Windows/FileManager/display.h
+++ Windows/FileManager/display.h
@@ -1,36 +1,39 @@
 #ifndef DISPLAY_H_
 #define DISPLAY_H_
 #include "directory.h"
-#include "texture.h"
 #include "shortcutbar.h"
 #include "shortcut.h"
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <string>
-#include <vector>
-#include <memory>
-#include <iostream>
 class Display {
 public:
 	Display(int width, int height);
 	~Display();
 
-	bool init();
+	/* put the Directory contentts on screen */
 	void renderDirectory(Directory&);
+	/* put all of the UI elements on the screen */
 	void renderUI(ShortcutBar &bar);
+	/* create textures for a Directory */
 	void buildDirectory(Directory& dir);
+	/* re-create textures for a Shortcut */
 	void buildShortcut(Shortcut& shortcut);
+	/* create textures for a ShortcutBar */
 	void buildShortcuts(ShortcutBar &bar);
+	/* display the screen */
 	void update();
-	SDL_Texture* createTextTexture(std::string text,
-		SDL_Color color,
-		SDL_Rect &pos);
+	
 private:
 	SDL_Texture* surfaceToTexture(SDL_Surface* surf);
 	SDL_Texture* surfaceToTextureSafe(SDL_Surface* surf);
+	SDL_Texture* createTextTexture(std::string text,
+					SDL_Color color,
+					SDL_Rect &pos);
 	void renderTexture(SDL_Texture*, SDL_Rect);
 	void renderCurrentPath();
 	void renderShortcuts(ShortcutBar &bar);
+	bool init();
 
 	SDL_Window* window;
 	SDL_Renderer* renderer;
